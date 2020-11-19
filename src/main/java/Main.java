053af4f14a9f5026ebb8799270f8bfbb636d9455
Lebedev Vyacheslav A <vylebede@megafon.ru>
2020-11-19 18:36:43 +0300

    fixed main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,15 @@
+import parrallelworkers.ParallelWorker;
+import parrallelworkers.ParallelWorkerImpl;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
 public class Main {
 
-    public static void main(String[] args){
-
+    public static void main(String[] args) throws InterruptedException, ExecutionException, IOException {
+        int numberFiles = Integer.parseInt(args[0]);
+        ParallelWorker parallelWorker = new ParallelWorkerImpl(numberFiles);
+        parallelWorker.executionStart();
     }
 
 }
